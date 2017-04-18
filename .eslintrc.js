e0435af89ee016c1e8733fb08b2db3a7949cb5d9
m-mik <m-mik@users.noreply.github.com>
2017-04-18 11:55:04 +0200

    Add babel, eslint, postcss config files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'extends': 'airbnb',
+  'plugins': [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
+    'jest': true,
+  },
+}
