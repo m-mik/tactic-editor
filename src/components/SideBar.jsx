846e8d58a194e705e01f62b9c254021de40c43be
m-mik <m-mik@users.noreply.github.com>
2017-05-01 01:53:08 +0200

    Add propTypes to SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SideBar = () => <div>SideBar</div>;
+const SideBar = ({ children }) => (
+  <aside>
+    {children}
+  </aside>
+);
+
+SideBar.defaultProps = {
+  children: null,
+};
+
+SideBar.propTypes = {
+  children: PropTypes.node,
+};
 
 export default SideBar;
