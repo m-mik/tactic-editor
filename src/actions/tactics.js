0873bfe6e6da1bb225c76add3f94cbb35d071913
m-mik <m-mik@users.noreply.github.com>
2017-05-03 12:47:46 +0200

    Move actions to seperate functions

--- src/actions/tactics.js
+++ src/actions/tactics.js
@@ -3,22 +3,28 @@ import * as types from '../constants/ActionTypes';
 import * as mockApi from '../api';
 import { tacticSchema } from '../constants/Schemas';
 
-export const fetchTactics = () => (dispatch) => {
-  dispatch({ type: types.FETCH_TACTICS_REQUEST });
+const fetchTacticsRequest = () => ({
+  type: types.FETCH_TACTICS_REQUEST,
+});
 
+const fetchTacticsSuccess = (ids, entities) => ({
+  type: types.FETCH_TACTICS_SUCCESS,
+  ids,
+  entities,
+});
+
+const fetchTacticsError = error => ({
+  type: types.FETCH_TACTICS_FAILURE,
+  error,
+});
+
+export const fetchTactics = () => (dispatch) => {
+  dispatch(fetchTacticsRequest);
   mockApi.fetchTactics().then((response) => {
     const normalizedData = normalize(response.data, { tactics: [tacticSchema] });
     const { entities, result } = normalizedData;
-
-    dispatch({
-      type: types.FETCH_TACTICS_SUCCESS,
-      ids: result.tactics,
-      entities,
-    });
+    dispatch(fetchTacticsSuccess(result.tactics, entities));
   }).catch((response) => {
-    dispatch({
-      type: types.FETCH_TACTICS_FAILURE,
-      error: response.data.error,
-    });
+    dispatch(fetchTacticsError(response.error));
   });
 };
