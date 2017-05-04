788fde64f8333efa039ccf5ab4e570278fcf081b
m-mik <m-mik@users.noreply.github.com>
2017-05-04 22:44:53 +0200

    Add NewTacticDialog actions and update ui reducer

--- src/actions/tactics.js
+++ src/actions/tactics.js
@@ -29,3 +29,6 @@ export const fetchTactics = () => (dispatch) => {
 };
 
 export const selectTactic = id => ({ type: types.SELECT_TACTIC, id });
+
+export const openNewTacticDialog = () => ({ type: types.OPEN_NEW_TACTIC_DIALOG });
+export const closeNewTacticDialog = () => ({ type: types.CLOSE_NEW_TACTIC_DIALOG });
--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -2,12 +2,17 @@ import * as types from '../constants/ActionTypes';
 
 const INITIAL_STATE = {
   selectedTacticId: -1,
+  newTacticDialogOpen: false,
 };
 
 const ui = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.SELECT_TACTIC:
       return { ...state, selectedTacticId: action.id };
+    case types.OPEN_NEW_TACTIC_DIALOG:
+      return { ...state, newTacticDialogOpen: true };
+    case types.CLOSE_NEW_TACTIC_DIALOG:
+      return { ...state, newTacticDialogOpen: false };
     default:
       return state;
   }
