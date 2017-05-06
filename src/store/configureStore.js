ec6e0543723ee53001d51fb331726c0732828172
m-mik <m-mik@users.noreply.github.com>
2017-05-06 23:04:53 +0200

    Add global-error-middleware to handle rejected http request promises

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
+import globalErrorMiddleware from '../middlewares/global-error-middleware';
 import normalizeMiddleware from '../middlewares/normalize-middleware';
 import reducers from '../reducers/index';
 
@@ -11,8 +12,9 @@ const configureStore = preloadedState => createStore(reducers,
     preloadedState,
     compose(
       applyMiddleware(thunkMiddleware),
-      applyMiddleware(normalizeMiddleware),
+      applyMiddleware(globalErrorMiddleware),
       applyMiddleware(promiseMiddleware()),
+      applyMiddleware(normalizeMiddleware),
       DEVELOPMENT && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
   );
