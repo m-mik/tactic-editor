f5a27fbc94eb0121e14150e26f9a4fec33916997
m-mik <m-mik@users.noreply.github.com>
2017-05-10 15:25:00 +0200

    Mock tactics/:id http requests

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import omit from 'lodash/omit';
+import pick from 'lodash/pick';
 import MockAdapter from 'axios-mock-adapter';
 import tactics from './tactics.json';
 
@@ -13,6 +14,12 @@ const mockApi = (store) => {
   };
 
   mock.onGet('/tactics').reply(200, tactics.map(tactic => omit(tactic, ['teams'])));
+
+  mock.onGet(/\/tactics\/\d+/).reply((config) => {
+    const id = +config.url.split('/').pop();
+    const tactic = tactics.filter(t => t.id === id).shift();
+    return [200, pick(tactic, ['id', 'teams'])];
+  });
 
   mock.onPost('/tactics').reply(() => [201, { id: getNextIdForEntity('tactics') }]);
 };
