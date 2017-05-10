cf15875f4f1af11dea4ae0eb9af2a277c03093bf
m-mik <m-mik@users.noreply.github.com>
2017-05-10 23:41:05 +0200

    Show Loading indicator when fetching tactic details

--- src/containers/tactics/TacticDetailContainer.jsx
+++ src/containers/tactics/TacticDetailContainer.jsx
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import * as tacticActions from '../../actions/tactics';
+import Loading from '../../components/Loading';
 
 class TacticDetailContainer extends Component {
+  componentDidMount() {
+    const id = +this.props.match.params.id;
+    this.props.selectAndFetchTactic(id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const nextId = +nextProps.match.params.id;
+    const currentId = +this.props.match.params.id;
+    if (nextId !== currentId) {
+      this.props.selectAndFetchTactic(nextId);
+    }
+  }
+
   render() {
+    const { errorOccurred, isFetching, selectedTacticId } = this.props;
     return (
       <section className="tactic-panel">
-          {/*<TacticEditor tacticId={this.props.selectedTacticId} />*/}
+        {isFetching && <Loading />}
+        {errorOccurred && <span className="error">Selected tactic does not exist</span>}
+        <div>selectedTacticId: {selectedTacticId}</div>
       </section>
     );
   }
 }
 
-const mapStateToProps = ({ ui }) => (
-  { selectedTacticId: ui.selectedTacticId }
-);
+const mapStateToProps = ({ ui, entities }) => {
+  const { tacticDetails } = entities;
+  const { status } = tacticDetails;
+  const { fetching, errors } = status;
+  const selectedTacticId = ui.selectedTacticId;
+
+  return {
+    selectedTacticId,
+    isFetching: fetching.indexOf(selectedTacticId) !== -1,
+    errorOccurred: errors.indexOf(selectedTacticId) !== -1,
+  };
+};
 
 TacticDetailContainer.propTypes = {
   selectedTacticId: PropTypes.number.isRequired,
+  selectAndFetchTactic: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  errorOccurred: PropTypes.bool.isRequired,
+  match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
-export default connect(mapStateToProps, null)(TacticDetailContainer);
+const ConnectedTacticDetailContainer = connect(
+  mapStateToProps,
+  tacticActions,
+)(TacticDetailContainer);
+
+export default withRouter(ConnectedTacticDetailContainer);
