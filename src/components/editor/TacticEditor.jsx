ee7c4c7d89a453782104f6f9bebd0b013b900c66
m-mik <m-mik@users.noreply.github.com>
2017-05-11 23:48:56 +0200

    Add tacticDetail selector

--- src/components/editor/TacticEditor.jsx
+++ src/components/editor/TacticEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TacticEditor = () => (
-  <div />
+  <div className="tactic-editor">Tactic Editor</div>
 );
 
 export default TacticEditor;
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,38 +1,39 @@
+import { denormalize } from 'normalizr';
+import includes from 'lodash/includes';
+import mapValues from 'lodash/mapValues';
 import { createSelector } from 'reselect';
+import { tacticDetailSchema } from '../constants/Schemas';
 
 const getTactics = state => state.entities.tactics;
 const getSelectedTacticId = state => state.ui.selectedTacticId;
 const getFetching = state => state.entities.tacticDetails.status.fetching;
 const getErrors = state => state.entities.tacticDetails.status.errors;
-
-const inArray = (array, value) => array.indexOf(value) !== -1;
+const getTeams = state => state.entities.teams;
+const getPlayers = state => state.entities.players;
+const getTacticDetails = state => state.entities.tacticDetails;
 
 export const tacticsSelector = createSelector(
   [getTactics], tactics => tactics.items.map(id => tactics.byId[id]),
 );
 
-/*
- import { denormalize } from 'normalizr';
- import { tacticSchema } from '../constants/Schemas';
-
- const getTeams = state => state.entities.teams;
-const getTacticDetails = state => state.entities.tacticDetails;
-
-export const tacticsDetailsSelector = createSelector(
-  [getTactics, getTeams],
-  (tactics, teams) => denormalize(
-    tactics.items, [tacticSchema],
-    { tactics: tactics.byId, teams: teams.byId },
-  ),
+export const tacticDetailSelector = createSelector(
+  [getTacticDetails, getTeams, getPlayers, getSelectedTacticId],
+  (tacticDetails, teams, players, selectedTacticId) => {
+    const entities = { tacticDetails, teams, players };
+    return denormalize(
+      selectedTacticId,
+      tacticDetailSchema,
+      { ...mapValues(entities, value => value.byId) },
+    );
+  },
 );
-*/
 
 export const isFetchingSelector = createSelector(
   [getFetching, getSelectedTacticId],
-  (fetching, selectedTacticId) => inArray(fetching, selectedTacticId),
+  (fetching, selectedTacticId) => includes(fetching, selectedTacticId),
 );
 
 export const hasErrorSelector = createSelector(
   [getErrors, getSelectedTacticId],
-  (errors, selectedTacticId) => inArray(errors, selectedTacticId),
+  (errors, selectedTacticId) => includes(errors, selectedTacticId),
 );
