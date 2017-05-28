2b248b49596aaeecbeca553d2902a8b1b3e7e65f
m-mik <m-mik@users.noreply.github.com>
2017-05-28 18:57:10 +0200

    Use different react-dnd backends for touch devices and desktops

--- src/containers/TeamGrid/withDragDropContext.js
+++ src/containers/TeamGrid/withDragDropContext.js
@@ -1,4 +1,8 @@
 import TouchBackend from 'react-dnd-touch-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 
-export default DragDropContext(TouchBackend({ enableMouseEvents: true }));
+const isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+const backend = isTouchDevice ? TouchBackend({ enableMouseEvents: true }) : HTML5Backend;
+
+export default DragDropContext(backend);
