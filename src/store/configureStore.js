9c706c71e0388cf4324ed773afe31b304903d70a
m-mik <m-mik@users.noreply.github.com>
2017-05-28 16:51:32 +0200

    Fix Redux devtool extension error for unsupported browsers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,14 +7,15 @@ import promiseMiddleware from 'redux-promise-middleware';
 import normalizeMiddleware from '../middlewares/normalizeMiddleware';
 import reducers from './reducers';
 
+const composeEnhancers = (DEVELOPMENT && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 const configureStore = preloadedState => createStore(reducers,
-    preloadedState,
-    compose(
-      applyMiddleware(thunkMiddleware),
-      applyMiddleware(promiseMiddleware()),
-      applyMiddleware(normalizeMiddleware),
-      DEVELOPMENT && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-  );
+  preloadedState,
+  composeEnhancers(
+    applyMiddleware(thunkMiddleware),
+    applyMiddleware(promiseMiddleware()),
+    applyMiddleware(normalizeMiddleware),
+  ),
+);
 
 export default configureStore;
