cd77973e82ce78b4841ef6a6d763beb359d88bb9
m-mik <m-mik@users.noreply.github.com>
2017-06-04 20:40:42 +0200

    Add EditTeamDialog constants and action creators

--- src/containers/TacticEditorPage/constants.js
+++ src/containers/TacticEditorPage/constants.js
@@ -1,6 +1,9 @@
 export const ADD_PLAYER_TRANSITIONS = 'tactic-editor/Editor/ADD_PLAYER_TRANSITIONS';
 export const REMOVE_PLAYER_TRANSITIONS = 'tactic-editor/Editor/REMOVE_PLAYER_TRANSITIONS';
 
+export const OPEN_EDIT_TEAM_DIALOG = 'tactic-editor/Editor/OPEN_EDIT_TEAM_DIALOG';
+export const CLOSE_EDIT_TEAM_DIALOG = 'tactic-editor/Editor/CLOSE_EDIT_TEAM_DIALOG';
+
 // export const MOVE_PLAYER = 'tactic-editor/Editor/MOVE_PLAYER';
 // export const SWAP_PLAYERS = 'tactic-editor/Editor/SWAP_PLAYERS';
 
