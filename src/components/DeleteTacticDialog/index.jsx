b275a649ee00486d6cc5c92ccdb883ef23ce2edd
m-mik <m-mik@users.noreply.github.com>
2017-06-12 23:06:03 +0200

    Add DeleteTacticDialog component

---
+++ src/components/DeleteTacticDialog/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import DialogLoadingIndicator from '../DialogLoadingIndicator';
+
+const DeleteTacticDialog = (props) => {
+  const { onDelete, onClose, open, pending, tactic } = props;
+
+  const actions = [
+    <FlatButton
+      label="Cancel"
+      primary
+      disabled={pending}
+      onTouchTap={onClose}
+    />,
+    <FlatButton
+      label="Delete"
+      type="submit"
+      secondary
+      disabled={pending}
+      onTouchTap={() => onDelete(tactic.id)}
+    />,
+  ];
+
+  return (
+    <Dialog
+      title={`Delete tactic "${tactic.name}"?`}
+      actions={actions}
+      modal={false}
+      open={open}
+      onRequestClose={onClose}
+    >
+      {pending && <DialogLoadingIndicator />}
+    </Dialog>
+  );
+};
+
+DeleteTacticDialog.propTypes = {
+  open: PropTypes.bool.isRequired,
+  pending: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  tactic: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default DeleteTacticDialog;
