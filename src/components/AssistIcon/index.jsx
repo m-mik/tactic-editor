aa2fd7422500a74ba2d15c5c27b5851dd16cb7b3
m-mik <m-mik@users.noreply.github.com>
2020-09-03 20:03:46 +0200

    Add BallIcon, GoalIcon and AssistIcon components

---
+++ src/components/AssistIcon/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { green500 } from 'material-ui/styles/colors';
+import BallIcon from '../BallIcon';
+
+const AssistIcon = props => (
+  <BallIcon color={green500} {...props} />
+);
+
+export default AssistIcon;
---
+++ src/components/GoalIcon/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { black } from 'material-ui/styles/colors';
+import BallIcon from '../BallIcon';
+
+const GoalIcon = props => (
+  <BallIcon color={black} {...props} />
+);
+
+export default GoalIcon;
